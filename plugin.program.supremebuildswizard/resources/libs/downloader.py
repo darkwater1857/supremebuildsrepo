c63bfd13806cabbef9e535b0864dbb652712f658
KodiSkills <KodiSkills@gmail.com>
2017-04-22 15:28:47 -0400

    downloader.py final
    show details

--- plugin.program.supremebuildswizard/resources/libs/downloader.py
+++ plugin.program.supremebuildswizard/resources/libs/downloader.py
@@ -18,7 +18,6 @@
 ################################################################################
 
 import xbmc, xbmcgui, urllib, sys, time, uservar
-from urllib import FancyURLopener
 import socket
 import wizard as wiz
 
@@ -28,9 +27,7 @@ ADDONTITLE     = uservar.ADDONTITLE
 COLOR1         = uservar.COLOR1
 COLOR2         = uservar.COLOR2
 
-class customdownload(urllib.FancyURLopener):
-    version = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
-
+urllib.URLopener.version = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
 
 def download(url, dest, dp = None):
 	if not dp:
@@ -38,25 +35,27 @@ def download(url, dest, dp = None):
 		dp.create(ADDONTITLE ,"Downloading Content",' ', ' ')
 	dp.update(0)
 	start_time=time.time()
-	customdownload().retrieve(url,dest,lambda nb, bs, fs, url=url: _pbhook(nb, bs, fs, dp, start_time))
-
+	urllib.urlretrieve(url, dest, lambda nb, bs, fs: _pbhook(nb, bs, fs, dp, start_time))
 
 def _pbhook(numblocks, blocksize, filesize, dp, start_time):
-        try: 
-            percent = min(numblocks * blocksize * 100 / filesize, 100) 
-            currently_downloaded = float(numblocks) * blocksize / (1024 * 1024) 
-            kbps_speed = numblocks * blocksize / (time.time() - start_time) 
-            if kbps_speed > 0: 
-                eta = (filesize - numblocks * blocksize) / kbps_speed 
-            else: 
-                eta = 0 
-            kbps_speed = kbps_speed / 1024 
-            mbps_speed = kbps_speed / 1024 
-            total = float(filesize) / (1024 * 1024) 
-            mbs = '[COLOR %s][B]Size:[/B] [COLOR %s]%.02f[/COLOR] MB of [COLOR %s]%.02f[/COLOR] MB[/COLOR]' % (COLOR2, COLOR1, currently_downloaded, COLOR1, total)
-            e = '[COLOR %s][B]Speed:[/B] [COLOR %s]%.02f [/COLOR]%s/s ' % (COLOR2, COLOR1, kbps_speed, type_speed)
-            e += '[B]ETA:[/B] [COLOR '+COLOR1+']%02d:%02d[/COLOR][/COLOR]' % divmod(eta, 60)
-            dp.update(percent, mbs, e)
+	try: 
+		percent = min(numblocks * blocksize * 100 / filesize, 100) 
+		currently_downloaded = float(numblocks) * blocksize / (1024 * 1024) 
+		kbps_speed = numblocks * blocksize / (time.time() - start_time) 
+		if kbps_speed > 0 and not percent == 100: 
+			eta = (filesize - numblocks * blocksize) / kbps_speed 
+		else: 
+			eta = 0
+		kbps_speed = kbps_speed / 1024 
+		type_speed = 'KB'
+		if kbps_speed >= 1024:
+			kbps_speed = kbps_speed / 1024 
+			type_speed = 'MB'
+		total = float(filesize) / (1024 * 1024) 
+		mbs = '[COLOR %s][B]Size:[/B] [COLOR %s]%.02f[/COLOR] MB of [COLOR %s]%.02f[/COLOR] MB[/COLOR]' % (COLOR2, COLOR1, currently_downloaded, COLOR1, total) 
+		e   = '[COLOR %s][B]Speed:[/B] [COLOR %s]%.02f [/COLOR]%s/s ' % (COLOR2, COLOR1, kbps_speed, type_speed)
+		e  += '[B]ETA:[/B] [COLOR '+COLOR1+']%02d:%02d[/COLOR][/COLOR]' % divmod(eta, 60)
+		dp.update(percent, '', mbs, e)
 	except Exception, e:
 		wiz.log("ERROR Downloading: %s" % str(e), xbmc.LOGERROR)
 		return str(e)
