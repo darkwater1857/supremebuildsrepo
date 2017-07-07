1601257fa0b5a8c85ae2b0638cc4b0bff704e9fa
KodiSkills <KodiSkills@gmail.com>
2017-07-07 01:50:26 -0400

    SB Wiz updated to SSL protocol v3.0.5

--- plugin.program.supremebuildswizard/uservar.py
+++ plugin.program.supremebuildswizard/uservar.py
@@ -5,21 +5,21 @@ import os, xbmc, xbmcaddon
 #########################################################
 ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
 ADDONTITLE     = 'Supreme Builds Wizard'
-EXCLUDES       = [ADDON_ID, 'repository.supremebuilds', 'repository.infadroid']
+EXCLUDES       = [ADDON_ID, 'repository.supremebuilds']
 # Text File with build info in it.
-BUILDFILE      = 'http://supreme1.srve.io/sbwiz/texts/builds.txt'
+BUILDFILE      = 'https://wizard.supremebuilds.com/texts/builds.txt'
 # How often you would list it to check for build updates in days
 # 0 being every startup of kodi
-UPDATECHECK    = 0
+UPDATECHECK    = 2
 # Text File with apk info in it.
-APKFILE        = 'http://supreme1.srve.io/sbwiz/texts/apks.txt'
+APKFILE        = 'https://wizard.supremebuilds.com/texts/apks.txt'
 # Text File with Youtube Videos urls.  Leave as 'http://' to ignore
 YOUTUBETITLE   = 'YouTube Videos'
-YOUTUBEFILE    = 'http://supreme1.srve.io/sbwiz/texts/youtube.txt'
+YOUTUBEFILE    = 'https://wizard.supremebuilds.com/texts/youtube.txt'
 # Text File for addon installer.  Leave as 'http://' to ignore
-ADDONFILE      = 'http://supreme1.srve.io/sbwiz/texts/addons.txt'
+ADDONFILE      = 'https://wizard.supremebuilds.com/texts/addons.txt'
 # Text File for advanced settings.  Leave as 'http://' to ignore
-ADVANCEDFILE   = 'http://supreme1.srve.io/sbwiz/texts/advanced.txt'
+ADVANCEDFILE   = 'https://wizard.supremebuilds.com/texts/advanced.txt'
 
 # Dont need to edit just here for icons stored locally
 PATH           = xbmcaddon.Addon().getAddonInfo('path')
@@ -34,17 +34,17 @@ ART            = os.path.join(PATH, 'resources', 'art')
 # Example:  ICONMAINT     = os.path.join(ART, 'mainticon.png')
 #           ICONSETTINGS  = 'http://aftermathwizard.net/repo/wizard/settings.png'
 # Leave as http:// for default icon
-ICONBUILDS     = 'http://supreme1.srve.io/sbwiz/images/supremebuilds.png'
-ICONMAINT      = 'http://supreme1.srve.io/sbwiz/images/maintenance.png'
-ICONAPK        = 'http://supreme1.srve.io/sbwiz/images/apkinstaller.png'
-ICONADDONS     = 'http://supreme1.srve.io/sbwiz/images/addoninstaller.png'
-ICONYOUTUBE    = 'http://supreme1.srve.io/sbwiz/images/youtube.png'
-ICONSAVE       = 'http://supreme1.srve.io/sbwiz/images/savedata.png'
-ICONTRAKT      = 'http://supreme1.srve.io/sbwiz/images/trakt.png'
-ICONREAL       = 'http://supreme1.srve.io/sbwiz/images/realdebrid.png'
-ICONLOGIN      = 'http://supreme1.srve.io/sbwiz/images/login.png'
-ICONCONTACT    = 'http://supreme1.srve.io/sbwiz/images/contactus.png'
-ICONSETTINGS   = 'http://supreme1.srve.io/sbwiz/images/settings.png'
+ICONBUILDS     = 'https://wizard.supremebuilds.com/images/supremebuilds.png'
+ICONMAINT      = 'https://wizard.supremebuilds.com/images/maintenance.png'
+ICONAPK        = 'https://wizard.supremebuilds.com/images/apkinstaller.png'
+ICONADDONS     = 'https://wizard.supremebuilds.com/images/addoninstaller.png'
+ICONYOUTUBE    = 'https://wizard.supremebuilds.com/images/youtube.png'
+ICONSAVE       = 'https://wizard.supremebuilds.com/images/savedata.png'
+ICONTRAKT      = 'https://wizard.supremebuilds.com/images/trakt.png'
+ICONREAL       = 'https://wizard.supremebuilds.com/images/realdebrid.png'
+ICONLOGIN      = 'https://wizard.supremebuilds.com/images/login.png'
+ICONCONTACT    = 'https://wizard.supremebuilds.com/images/contactus.png'
+ICONSETTINGS   = 'https://wizard.supremebuilds.com/images/settings.png'
 # Hide the ====== seperators 'Yes' or 'No'
 HIDESPACERS    = 'No'
 # Character used in seperator
@@ -55,7 +55,7 @@ SPACER         = '='
 COLOR1         = 'firebrick'
 COLOR2         = 'ghostwhite'
 # Primary menu items   / %s is the menu item and is required
-THEME1         = '[COLOR '+COLOR1+'][B][I]([COLOR '+COLOR2+']Supreme Builds[/COLOR])[/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR][/I]'
+THEME1         = '[COLOR '+COLOR1+'][B][I]([COLOR '+COLOR2+']Aftermath[/COLOR])[/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR][/I]'
 # Build Names          / %s is the menu item and is required
 THEME2         = '[COLOR '+COLOR2+']%s[/COLOR]'
 # Alternate items      / %s is the menu item and is required
@@ -69,10 +69,10 @@ THEME5         = '[COLOR '+COLOR1+']Current Theme:[/COLOR] [COLOR '+COLOR2+']%s[
 # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
 HIDECONTACT    = 'No'
 # You can add \n to do line breaks
-CONTACT        = 'Thank you for choosing Supreme Builds.\r\n\r\nContact us and get support on facebook at http://facebook.com/groups/supremebuilds'
+CONTACT        = 'Thank you for choosing Supreme Builds.\r\n\r\nContact us on facebook at https://facebook.com/groups/supremebuilds'
 #Images used for the contact window.  http:// for default icon and fanart
-CONTACTICON    = 'http://supreme1.srve.io/sbwiz/images/contactus.png'
-CONTACTFANART  = 'http://supreme1.srve.io/sbwiz/images/sbforumfanart.jpg'
+CONTACTICON    = 'https://wizard.supremebuilds.com/images/contactus.png'
+CONTACTFANART  = 'https://wizard.supremebuilds.com/images/sbforumfanart.jpg'
 #########################################################
 
 #########################################################
@@ -103,12 +103,12 @@ REPOZIPURL     = 'https://github.com/kodiskills/supremebuildsrepo/raw/master/zip
 # Enable Notification screen Yes or No
 ENABLE         = 'Yes'
 # Url to notification file
-NOTIFICATION   = 'http://supreme1.srve.io/sbwiz/texts/notify.txt'
+NOTIFICATION   = 'https://wizard.supremebuilds.com/texts/notify.txt'
 # Use either 'Text' or 'Image'
 HEADERTYPE     = 'Image'
 HEADERMESSAGE  = ''
 # url to image if using Image 424x180
-HEADERIMAGE    = 'http://supreme1.srve.io/sbwiz/images/sbnews.png'
+HEADERIMAGE    = 'https://wizard.supremebuilds.com/images/sbnews.png'
 # Background for Notification Window
-BACKGROUND     = 'http://supreme1.srve.io/sbwiz/images/sbsplash.jpg'
+BACKGROUND     = 'https://wizard.supremebuilds.com/images/sbsplash.jpg'
 #########################################################
